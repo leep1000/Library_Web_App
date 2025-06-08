c25437616aed99d572856d903e825effc64453c9
Lee Palmer <leepalmerformal@gmail.com>
2025-06-09 00:18:37 +0100

    added initial config file

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Create the SQLAlchemy database instance 
+db = SQLAlchemy()
+
+def create_app():
+    # Create the Flask app object
+    app = Flask(__name__)
+
+    # Set secret key and database location 
+    app.config['SECRET_KEY'] = 'secretkey123'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+    # Connect database to this Flask app
+    db.init_app(app)
+
+    # Import routing for the app
+    with app.app_context():
+        # Import models to ensure they are registered with SQLAlchemy
+        from . import models  
+        # Create the database tables if they do not exist
+        db.create_all()
+        
+    from . import routes  
+
+    return app
